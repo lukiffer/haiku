e71db1205e73bc5aa3687f66edb9d9edc9a9d88f
Luke Fritz <lukefritz@gmail.com>
2018-08-23 00:10:11 -0500

    Add haiku settings dialog

--- src/elements/haiku-tile-base.js
+++ src/elements/haiku-tile-base.js
@@ -1,17 +1,46 @@
 import { LitElement } from 'https://unpkg.com/@polymer/lit-element@^0.5.2/lit-element.js?module';
 import { EventService } from '../services/event-service.js';
+import '../elements/haiku-sensor-settings-dialog.js';
 
 export class HaikuTileBase extends LitElement {
+  constructor() {
+    super();
+    this.dialog = this._findMoreInfoDialog();
+  }
+
   handleClick(event) {
     event.stopPropagation();
+    const eventService = new EventService();
     if (event.altKey) {
-      console.log('TODO: show input dialog...');
+      this.dialog = this._findMoreInfoDialog();
+      this.dialog.fire('more-info-page', { page: 'haiku_settings' });
+      const sensorSettingsDialog = document.createElement('haiku-sensor-settings-dialog');
+      sensorSettingsDialog.entity = this.entity;
+      this.dialog.shadowRoot.appendChild(sensorSettingsDialog);
+      this.dialog.open();
+      // debugger; // eslint-disable-line no-debugger
+      this.dialog.addEventListener('iron-overlay-canceled', () => {
+        this._handleDialogCancel();
+        this.dialog.removeEventListener(this._handleDialogCancel);
+        this.dialog.removeEventListener('iron-overlay-canceled');
+      });
     }
     else {
-      const eventService = new EventService();
       eventService.fire(event.target, 'hass-more-info', {
         entityId: this.entity.entity_id
       });
     }
+  }
+
+  _findMoreInfoDialog() {
+    const hassEl = document.getElementsByTagName('home-assistant')[0];
+    const hassMainEl = hassEl.shadowRoot.querySelector('home-assistant-main');
+    return hassMainEl.shadowRoot.querySelector('ha-more-info-dialog');
+  }
+
+  _handleDialogCancel() {
+    const el = this.dialog.shadowRoot.querySelector('haiku-sensor-settings-dialog');
+    this.dialog.fire('more-info-page', { page: null });
+    this.dialog.shadowRoot.removeChild(el);
   }
 }
