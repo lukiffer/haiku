2a17167bcc9123122cbe03e2937cfbd21140391e
Luke Fritz <lukefritz@gmail.com>
2018-09-01 23:42:56 -0400

    Add support for SSH port number

--- deploy.sh
+++ deploy.sh
@@ -1,1 +1,1 @@
-rsync -aviO ./haiku/ $HA_SSH_USER@$HA_SSH_HOST:/home/homeassistant/.homeassistant/www/haiku/
+rsync -aviO -e "ssh -p $HA_SSH_PORT -o StrictHostKeyChecking=no" ./haiku/ $HA_SSH_USER@$HA_SSH_HOST:/home/homeassistant/.homeassistant/www/haiku/
